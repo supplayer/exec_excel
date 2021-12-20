4d121ab0db52c106894503ecfdae4e23b08565b4
supplayer <x254724521@hotmail.com>
2021-12-21 02:14:19 +0800

    add new class Coroutine

--- exectools/__init__.py
+++ exectools/__init__.py
@@ -2,6 +2,7 @@ from exectools.handler import ExecImport, ExecExport
 from exectools.tools_basic import Tools
 from exectools.tools_excel import ExecExcel
 from exectools.tools_decorator import Retry, RunTime
+from exectools.tools_coroutine import Coroutine
 
 
 __all__ = [
@@ -11,4 +12,5 @@ __all__ = [
     'Tools',
     'Retry',
     'RunTime',
+    'Coroutine',
 ]
