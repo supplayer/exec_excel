bf352b21443a412e78b1edc7f9efef2a3b9952b7
supplayer <x254724521@hotmail.com>
2021-12-21 21:36:56 +0800

    optimize Coroutine.run_all add raise_error

--- exectools/tools_coroutine.py
+++ exectools/tools_coroutine.py
@@ -7,14 +7,14 @@ class Coroutine:
         self.batch_size = batch_size
         self.sleep = sleep
 
-    def run_all(self, func, func_args: List[dict], batch_size=None, sleep=None):
+    def run_all(self, func, func_args: List[dict], batch_size=None, sleep=None, raise_error=True, **kwargs):
         for i in self.item_range(func_args, batch_size):
             workers = [self.__handler.spawn_later(sleep or self.sleep, func, **j) for j in func_args[i[0]:i[1]]]
-            self.__handler.joinall(workers)
+            self.__handler.joinall(workers, raise_error=raise_error, **kwargs)
             yield [res.value for res in workers]
 
-    def item_range(self, items: List[dict], batch_size=None):
+    def item_range(self, items: list, batch_size=None):
         batch_siez = batch_size or self.batch_size
         for i in range(0, len(items), batch_siez):
             cursor = i
-            yield cursor, cursor+batch_siez
+            yield cursor, cursor + batch_siez
