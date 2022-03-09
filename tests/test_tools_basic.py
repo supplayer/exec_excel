965d51def1b962215833802531d1c495f1efe90a
supplayer <x254724521@hotmail.com>
2022-03-09 17:23:00 +0800

    optimize exectools.tools_basic.TimeSection.nowstamp_point kwargs name

--- tests/test_tools_basic.py
+++ tests/test_tools_basic.py
@@ -4,5 +4,5 @@ from time import time
 
 if __name__ == '__main__':
     print(datetime.now())
-    print(TimeSection.nowstamp_point(time_type='hours', time_delta=1))
+    print(TimeSection.nowstamp_point(time_type='hours', time_type_offset=1))
     print(TimeSection.timestamp_point(time(), "hours"))
